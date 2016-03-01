4c3833a690ca83071104cb255eeca18428324b36
erik <erik@erik-PC>
2016-03-01 20:49:24 +0000

    Hola

---
+++ src/HOLAMUNDO.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HOLAMUNDO {
+
+	public static void main(String[] args) {
+		
+		Scanner obj = new Scanner (System.in);
+		
+		System.out.println("Hola mundo");
+		
+		obj.close();
+		
+	}
+
+}
